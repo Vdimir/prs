58c617de9b66d90967af55a298cb81fdf4a59c64
VDimir <vcherckasov@yandex.ru>
2016-02-27 12:11:00 +0300

    Node::Iden

--- tests/expr.rs
+++ tests/expr.rs
@@ -25,17 +25,18 @@ enum ExprToken {
 }
 
 impl ExprToken {
-    fn is_mul_op(&self) -> bool {
-        match *self {
-            ExprToken::Mul | ExprToken::Div => true,
-            _ => false,
-        }
-    }
-
-    fn num(&self) -> u32 {
-        match *self {
+    fn num(self) -> u32 {
+        match self {
             ExprToken::Num(n) => Ok(n),
             _ => Err("not a number"),
+        }
+        .unwrap()
+    }
+
+    fn iden(self) -> String {
+        match self {
+            ExprToken::Iden(s) => Ok(s),
+            _ => Err("not a identifier"),
         }
         .unwrap()
     }
@@ -47,15 +48,28 @@ impl ExprToken {
         }
     }
 
+    fn is_iden(&self) -> bool {
+        match *self {
+            ExprToken::Iden(_) => true,
+            _ => false,
+        }
+    }
+
     fn is_add_op(&self) -> bool {
         match *self {
             ExprToken::Add | ExprToken::Sub => true,
             _ => false,
         }
     }
+
+    fn is_mul_op(&self) -> bool {
+        match *self {
+            ExprToken::Mul | ExprToken::Div => true,
+            _ => false,
+        }
+    }
 }
 
-use std::collections::LinkedList;
 fn tokenize(input: &str) -> Result<Vec<ExprToken>, ParseErr<char>> {
     let stream = &mut CharStream::new(input);
 
@@ -134,12 +148,14 @@ type BoxPair<T> = Box<(T, T)>;
 #[derive(PartialEq, Debug)]
 enum Node {
     Num(u32),
+    Iden(String),
     Add(BoxPair<Node>),
     Sub(BoxPair<Node>),
     Mul(BoxPair<Node>),
     Div(BoxPair<Node>),
 }
 
+use std::collections::HashMap;
 impl Node {
     fn create_op(op: ExprToken, lhs: Node, rhs: Node) -> Self {
         match op {
@@ -151,38 +167,24 @@ impl Node {
         }
     }
 
-    fn calc(&self) -> u32 {
+    fn calc(&self, vars: &HashMap<String, u32>) -> u32 {
         use Node::*;
 
         match *self {
             Num(n) => n,
             // with box_pattern:
             // Add(box (ref a, ref b)) => a.calc() + b.calc(),
-            Add(ref pair) => pair.0.calc() + pair.1.calc(),
-            Sub(ref pair) => pair.0.calc() - pair.1.calc(),
-            Mul(ref pair) => pair.0.calc() * pair.1.calc(),
-            Div(ref pair) => pair.0.calc() / pair.1.calc(),
+            Add(ref pair) => pair.0.calc(vars) + pair.1.calc(vars),
+            Sub(ref pair) => pair.0.calc(vars) - pair.1.calc(vars),
+            Mul(ref pair) => pair.0.calc(vars) * pair.1.calc(vars),
+            Div(ref pair) => pair.0.calc(vars) / pair.1.calc(vars),
+            Iden(ref s) => *vars.get(s).unwrap(),
         }
     }
 }
 
 fn parse_expr(s: &str) -> Result<Node, ParseErr<ExprToken>> {
-    let tokens = &mut VecStream::new(tokenize(s).unwrap());
-
-    fn factor_p(s: &mut VecStream<ExprToken>) -> Result<Node, ParseErr<ExprToken>> {
-        use ExprToken::*;
-
-        let num = predicate(|t: &ExprToken| t.is_num())
-                    .then(|t: ExprToken| Node::Num(t.num()) );
-
-        let parens_exp =
-            (Token(LParen),
-            fn_parser(expression),
-            Token(RParen))
-            .then(|(_, e, _)| e);
-
-        num.or(parens_exp).parse(s)
-    }
+    let tokens: &mut VecStream<ExprToken> = &mut VecStream::new(tokenize(s).unwrap());
 
     fn list_to_tree((mut node, rest): (Node, Option<Vec<(ExprToken, Node)>>)) -> Node {
         for (op, rh) in rest .unwrap_or(vec![]) .into_iter() {
@@ -191,20 +193,31 @@ fn parse_expr(s: &str) -> Result<Node, ParseErr<ExprToken>> {
         node
     }
 
-    fn inf_mul_f(s: &mut VecStream<ExprToken>) -> Result<Node, ParseErr<ExprToken>> {
-        let factor = fn_parser(factor_p);
-        let op_symb = predicate(|t: &ExprToken| t.is_mul_op());
-
-        (&factor, maybe(many((op_symb, &factor))))
-        .then(list_to_tree)
-        .parse(s)
-    }
-
     fn expression(s: &mut VecStream<ExprToken>) -> Result<Node, ParseErr<ExprToken>>
     {
-        let inf_mul = fn_parser(inf_mul_f);
+        let num = predicate(|t: &ExprToken| t.is_num())
+                    .then(|t: ExprToken| Node::Num(t.num()) );
+
+        let iden = predicate(|t: &ExprToken| t.is_iden())
+                    .then(|t: ExprToken| Node::Iden(t.iden()) );
+
+        let parens_exp =
+            (Token(ExprToken::LParen),
+            fn_parser(expression),
+            Token(ExprToken::RParen))
+            .then(|(_, e, _)| e);
+
+        let factor = num.or(iden).or(parens_exp);
+        let op_symb = predicate(|t: &ExprToken| t.is_mul_op());
+
+        let inf_mul = fn_parser(move |s| {
+            (&factor, maybe(many((&op_symb, &factor))))
+                .then(list_to_tree)
+                .parse(s)
+        });
+
         let add_symb = predicate(|t: &ExprToken| t.is_add_op());
-        let expr_parser = (&inf_mul, (add_symb, &inf_mul)
+        let expr_parser  = (&inf_mul, (add_symb, &inf_mul)
                                                .many().maybe())
                     .then(list_to_tree);
         expr_parser.parse(s)
@@ -244,8 +257,13 @@ fn expr_test() {
                                      Num(9))),
                             Num(3)))))));
 
-    let ast = parse_expr("5+8/4+3*(9+4-2*3)-((2+3))-8+(2+3)").unwrap();
-    assert_eq!(ast.calc(), 20);
+    let mut variables = HashMap::new();
+    variables.insert("zero".to_owned(), 0);
+    variables.insert("one".to_owned(), 1);
+    variables.insert("eight".to_owned(), 8);
+
+    let ast = parse_expr("5+ eight /4+3*(9+4-2*3)-((2+3))-8+(2+3)").unwrap();
+    assert_eq!(ast.calc(&variables), 20);
 
     assert_eq!(parse_expr("(3").ok(), None);
     assert_eq!(parse_expr("1+5 9").ok(), None);
